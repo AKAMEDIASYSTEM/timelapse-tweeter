31859d8e78a8117647ae08a0463fcdc57d0b8614
AKA <nfeehan@gmail.com>
2016-10-16 11:44:50 -0400

    crontab error duh

--- install.sh
+++ install.sh
@@ -5,5 +5,5 @@ sudo chmod +x upload.sh catchup.sh takestill.sh
 line="* * * * * cd /home/pi/timelapse-tweeter && sudo sh takestill.sh"
 (sudo crontab -l; echo "$line" ) | sudo crontab -
 # ALTER THIS LINE TO CHANGE HOW OFTEN YOU UPLOAD A GIF
-line="* 31 * * * cd /home/pi/timelapse-tweeter && sudo sh upload.sh"
+line="31 * * * * cd /home/pi/timelapse-tweeter && sudo sh upload.sh"
 (sudo crontab -l; echo "$line" ) | sudo crontab -
